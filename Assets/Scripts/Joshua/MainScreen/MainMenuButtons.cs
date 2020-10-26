561064c6b554ab052507c53f5864411ce86e2106
Joshua <jodaschenkel@gmail.com>
2020-10-26 22:04:34 +0100

    MainMenu
    See what happens

---
+++ Assets/Scripts/Joshua/MainScreen/MainMenuButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] private int _cardScene;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(_cardScene);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Quit();
+    }
+
+    public void Quit()
+    {        
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/Ollie/ResourceManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static ResourceManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("There are two resourceManager scripts and this will cause a lot of problems, please remove one of them");
+        }
+
+        hunger = startHunger;
+        thirst = startThirst;
+        shelter = startShelter;
+        panic = startPanic;
+    }
+
+    #endregion
+
+    [Header("UI Referrences")]
+    [SerializeField] TextMeshProUGUI hungerText;
+    [SerializeField] TextMeshProUGUI thirstText;
+    [SerializeField] TextMeshProUGUI shelterText;
+    [SerializeField] TextMeshProUGUI panicText;
+
+    [Header("Resources")]
+    [SerializeField] int startHunger;
+    [SerializeField] int startThirst;
+    [SerializeField] int startShelter;
+    [SerializeField] int startPanic;
+
+    public int hunger;
+    public int thirst;
+    public int shelter;
+    public int panic;
+
+
+
+    public void UpdateResourcesDisplay()
+    {
+        hungerText.text = "Hunger: " + hunger.ToString(); 
+        thirstText.text = "Thirst: " + thirst.ToString(); 
+        shelterText.text = "Shelter: " + shelter.ToString(); 
+        panicText.text = "Panic: " + panic.ToString(); 
+    }
+
+}
